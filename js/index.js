6c4696ccde656e4290e49c056106619fc8e2d6a7
653579487 <605895665@qq.com>
2017-02-15 23:38:03 +0800

    '写的好乱没有思路'

--- js/index.js
+++ js/index.js
@@ -8,17 +8,12 @@ $(function(){
       /*鼠标一如nav*/
       $(".showdiv .list-nav").eq($(this).index()).css('display','block').siblings().hide()
   });
-
-
     /*鼠标离nav开盒子*/
     $('#nav').mouseleave(function(){
         $(".showdiv .list-nav").css('display','none');
     });
-
-
   //轮播图不会写 拼的 只限主流浏览器使用
         var num = 0;
-
         var aa = setInterval(function(){
              num++;
             num%=4;
@@ -29,15 +24,12 @@ $(function(){
                     .siblings('li').removeClass('active');
             })
         },4000);
-
-
     /*轮播图的小点*/
     $('.container .animate .animate-last>li').on('click',function(){
         clearInterval(aa);
         $('.container .animate #lbt').css({'margin-left':$(this).index()*-1200+'px'});
         $(this).addClass('active').siblings('li').removeClass('active');
     });
-
     $('.container .animate #lbt').mouseenter(function(){
         clearInterval(aa);
     }).mouseleave(function(){
